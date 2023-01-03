bd34c5eaa9b1f87377b5c3fd5a9a61e9558b7418
BaurzhanA <almaty02r@gmail.com>
2023-01-04 00:49:55 +0600

    Add sql scripts for creating tables

--- sql/Init.sql
+++ sql/Init.sql
@@ -1,17 +1,31 @@
 //sql scripts for creating tables for MonthLimit and ExpenseTransaction
 
 CREATE DATABASE solva;
-GRANT ALL PRIVILEGES ON DATABASE  "solva" TO postgres;
 
-CREATE TABLE month_limit(
-    m_limit_id SERIAL PRIMARY KEY,
-    month_limit NUMERIC,
-    currency ENUM('RUB', 'USD', 'EUR')
+CREATE TABLE `daily_exchange_report` (
+  `der_id` int NOT NULL AUTO_INCREMENT,
+  `close` decimal(19,5) DEFAULT NULL,
+  `date_time` datetime(6) DEFAULT NULL,
+  `exchange` varchar(255) DEFAULT NULL,
+  `symbol` varchar(255) DEFAULT NULL,
+  `timestamp` bigint NOT NULL,
+  PRIMARY KEY (`der_id`)
 );
 
-CREATE TABLE expense_transaction(
-    e_tran_id SERIAL PRIMARY KEY,
-    currency ENUM('RUB', 'USD', 'EUR'),
-    name VARCHAR
+CREATE TABLE `expense_transaction` (
+  `e_tran_id` int NOT NULL AUTO_INCREMENT,
+  `currency` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `date_time` datetime(6) DEFAULT NULL,
+  `limit_exceeded` bit(1) DEFAULT NULL,
+  `sum` decimal(19,2) DEFAULT NULL,
+  PRIMARY KEY (`e_tran_id`)
+);
 
+CREATE TABLE `month_limit` (
+  `m_limit_id` int NOT NULL AUTO_INCREMENT,
+  `currency` varchar(255) DEFAULT NULL,
+  `month_limit` decimal(19,2) DEFAULT NULL,
+  `month` int DEFAULT NULL,
+  PRIMARY KEY (`m_limit_id`)
 );
